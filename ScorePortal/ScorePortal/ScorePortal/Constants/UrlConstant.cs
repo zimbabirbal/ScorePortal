0f74568af570ae5a2fbfcd7dbd66d2f969e86345
zimbabirbal <tamangbirbal23@gmail.com>
2020-01-02 00:22:00 +0545

    add team

---
+++ ScorePortal/ScorePortal/ScorePortal/Constants/UrlConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScorePortal.Constants
+{
+    public class UrlConstant
+    {
+        public const string BaserUrl = "http://scp.appsthatcrash.com";
+        //Team Request
+        public const string TeamGetRequest = "api/Team/{0}";
+    }
+}
--- ScorePortal/ScorePortal/ScorePortal/Views/AddTeam.xaml.cs
+++ ScorePortal/ScorePortal/ScorePortal/Views/AddTeam.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ScorePortal.Models;
+﻿using RestSharp;
+using ScorePortal.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -41,6 +42,35 @@ namespace ScorePortal.Views
             PlayerList.RemoveAt(e.ItemIndex);
             playerList.ItemsSource = PlayerList;
             playerList.HeightRequest = playerListRowHeight * PlayerList.Count;
+        }
+
+        private async void addTeam_Clicked(object sender, EventArgs e)
+        {
+            var client = new RestClient(Constants.UrlConstant.BaserUrl);
+
+            var request = new RestRequest(string.Format(Constants.UrlConstant.TeamGetRequest, a), DataFormat.Json);
+
+            var response = await client.GetAsync<Team>(request);
+        }
+        private async void GetTeamAsync()
+        {
+            var a = 1;
+            var client = new RestClient(Constants.UrlConstant.BaserUrl);
+
+            var request = new RestRequest(string.Format(Constants.UrlConstant.TeamGetRequest, a), DataFormat.Json);
+            request.AddParameter("SportId", 1);
+            request.AddParameter("Name", "");
+            request.AddParameter("ImageUrl", "");
+            request.AddParameter("Description", "ssfd");
+
+            var response = await client.PostAsync<Team>(request);
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(response.Id + "/n");
+            stringBuilder.Append(response.SportId + "/n");
+            stringBuilder.Append(response.Name + "/n");
+            stringBuilder.Append(response.ImageUrl + "/n");
+            stringBuilder.Append(response.Description + "/n");
+            httpresponse.Text = stringBuilder.ToString();
         }
     }
 }
